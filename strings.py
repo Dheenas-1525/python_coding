6c3a91534bef87d67f2a2334b173e4f1f3986990
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-03-25 22:42:54 +0530

    updated code

---
+++ strings.py
@@ -0,0 +1,32 @@
+"""
+first 256 number computer have static memory address.
+don't use space in string
+"""
+
+#sring-imutable:
+a="hello world"
+print(a[2])
+
+print(id(a))
+
+a="hello world"
+b="helloworld"
+c="hello_world"
+d="hello@world"
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+
+#during this above expriment string have memory address so it is called imutubale datatype.
+
+#string concordination:
+
+name="dheena"
+msg=f"hi {name} welome to python programming {name}" #f means format this is used in all lasted python version 
+print(msg)
+
+#old versio of python string format:
+
+msg="welcome to python programming {name}".format(name=name)
+print(msg)
