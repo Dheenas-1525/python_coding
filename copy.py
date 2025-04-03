61995bd19604635c0299075e149c18c5560cc93e
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-03 23:32:59 +0530

    sicling

---
+++ copy.py
@@ -0,0 +1,8 @@
+#this is using silcing and buildin copy function in python 
+d=['dheena']
+c=d.copy() #this is called as rapper function
+print(d)
+print(c)
+
+e=d[::1]
+print(e) #this is called as sicling in python 
