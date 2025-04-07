04c0a027011afac6b0baf784dc2b7895a5b271cc
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-07 16:49:21 +0530

    add some code

---
+++ function.py
@@ -0,0 +1,5 @@
+#this is my first python fucntion code 
+def addtwo(a,b):
+	c=(a+b)
+	return c
+print(addtwo(5,5))
---
+++ ownflames.py
@@ -0,0 +1,4 @@
+a=input("enter your name:\n")
+b-input("enter your partner name:\n")
+total=len(a)+len(b)
+flames=['friends','love','affection','marriage','enimies','sister']
