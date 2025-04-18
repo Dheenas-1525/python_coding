d899fec7c7bd4b0b59709c30b273b2fcf0be1d8f
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-18 15:02:13 +0530

    complex

---
+++ complex.py
@@ -0,0 +1,32 @@
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(z) 
+
+
+a="56"
+print(a)
+
+b=" dheena dhayalan"
+print(b.strip())
+
+c='subraja'
+d='sridhar'
+e=c+d
+print(e)
+
+a="dheena"
+d=f"welcome to this world {a} nice to see u!"
+print(d)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+txt = "\110\145\154\154\157"
+print(txt) 
+
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
---
+++ stringmethods.py
@@ -0,0 +1,23 @@
+#this is to test code of how string methods are used in python!
+# first method is capitalize() it is used to upper case all first letter of sentence
+txt="hello dheena welcome to this python world nice to meet you buddy.."
+x=txt.capitalize()
+print(x)
+
+txt = "36 is my age."
+
+x = txt.capitalize()
+
+print (x) 
+
+txt = "python is FUN!"
+
+x = txt.capitalize()
+
+print (x) 
+
+#second one is casefold this method is used for to lowercase of string 
+txt="DHEENA LOVE SUBRAJA"
+x=txt.casefold()
+print(x)
+
