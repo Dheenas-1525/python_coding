c5ddac567d7bbaf157500f9cd8b862f86d77b9f3
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-21 15:49:43 +0530

    control statemnets

--- collections.py
+++ collections.py
@@ -110,11 +110,31 @@ es=set()
 print(es)
 es={1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9}
 print(es)
-es={1,2,3,4,5,6,7,8,9}
-es1={9,8,7,6,5,4,3,2,1}
+es={1,3,4,5,6,}
+es1={9,8,7,5,3,}
 print(es & es1)
 print(es | es1)
-if es(6) in ['7']:
-	print("true")
-else:
-	print("false")
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset)) 
+
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False} 
+
+print(set1)
+print(set2)
+print(set3)
+
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
---
+++ controlstatements.py
@@ -0,0 +1,38 @@
+#this code used for to test and use case of python control structure!
+#syntax of if-ellse statement..
+"""
+go the  shop(){
+if(milkava()){
+buy milk()
+}
+if(eggava()){
+bug egg()
+}
+.......................
+this above sudo code is if statement 
+this is below sudo code is used for if-elif statement
+
+if(milk ava())
+{
+bug milk()
+}
+elif(egg ava())
+bug egg()
+}
+else()
+{
+dont bug anything
+}
+this is how sudo code works in python if else control statements.."""
+#sample code:
+#store=['milk','vegitables','eggs']
+store=[]
+if 'milk' in store:
+	print("buy milk")
+elif 'eggs' in store:
+	print("bug eggs")
+elif 'vegitables' in store:
+	print("buy vegitables")
+else:
+	print("nothing is avaiable in store")
+
