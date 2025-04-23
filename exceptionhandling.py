0260e78dc66d881a1e8c5af9cf6ed9e4cb0c4057
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-23 16:26:13 +0530

    exception handling

---
+++ exceptionhandling.py
@@ -0,0 +1,7 @@
+'''this code is look at exception handling it is used to through errors not in code error it totally outside error
+like if download file from internet if internet disconnected it show connect the your network like that of error 
+there are four type of errror 
+1)try
+2)exception
+3)else
+4)finally these are that four types exception handling in python '''
