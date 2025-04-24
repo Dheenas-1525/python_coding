8ff593cd8da4b249fb14567824f69d67827fb29e
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-24 12:00:05 +0530

    myexception

--- exceptionclass.py
+++ exceptionclass.py
@@ -1,11 +1,23 @@
 #this code sibi bro worked in class video
 import sys
 a = 10
-b = [1,2,3,4]
-c = 5
+b = [1,2,3,4,5]
+c = 2
 #first one is use to try 
 try:
     c = a + b[c]
+    raise IOError("this is sample error")
+    print("value of c is {}".format(c))
 except NameError as e:
-    print("name error happend")
-    print(e)
+    print("name error happend : {}".format(e))
+    # print(e)
+except IndexError as e:
+    print("index error happend : {}".format(e))
+    # print(e)
+except Exception as e:
+    print("something else : {}".format(e))
+    print("error: {} ".format(+sys.exc_info()[0]))
+else: 
+    print("all good at this point")
+finally:
+    print("this will print always") 
---
+++ myexception.py
@@ -0,0 +1,15 @@
+#this code is my pov of exception handling:
+a = 10
+b = [1,2,3,4]
+c = 5
+try:
+    c = a + b[7]
+    print(c)
+except IndexError as d:
+    print("this is index error... : {}".format(d))
+except NameError as d:
+    print("this is name error : {}".format(d))
+else:
+    print("all going good")
+finally:
+    print("see the error good..")
