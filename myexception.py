5bc06796a91aa6733aba1f988ba98b1c374f3913
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-24 12:25:11 +0530

    sibi bro code

--- myexception.py
+++ myexception.py
@@ -3,7 +3,8 @@ a = 10
 b = [1,2,3,4]
 c = 5
 try:
-    c = a + b[7]
+    c = a + b[2]
+    # raise IOError("this is sample error...")
     print(c)
 except IndexError as d:
     print("this is index error... : {}".format(d))
---
+++ sibi_bro_exception.py
@@ -0,0 +1,26 @@
+#this code is used in class videos
+import sys
+a = 10
+b = [1,2,3,4]
+c = 2
+
+def divide(a,b):
+    if b == 0:
+        raise Exception("cannot divide by zero")
+    return a+b
+try:
+    c = divide(5,5)
+    print("value of c is : {}".format(c))
+except NameError as e:
+    print("name error happend : {}".format(e))
+    # print(e)
+except IndexError as e:
+    print("index error happend : {}".format(e))
+    # print(e)
+except Exception as e:
+    print("something else : {}".format(e))
+    print("error: {} ".format(sys.exc_info()))
+else: 
+    print("all good at this point")
+finally:
+    print("whatever the case is , i will work") 
