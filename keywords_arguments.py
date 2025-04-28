3a95dce94634ecc41a87cc9e1aa53dd20469f0de
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-28 11:42:12 +0530

    add varibale scopes

--- keywords_arguments.py
+++ keywords_arguments.py
@@ -1,4 +1,5 @@
 """this code shown the demo of how keywords and positional arguments worked in python"""
+#varibales number in positional argumenst:
 def add(x, *y):
     print(x)
     print(y)
@@ -18,9 +19,30 @@ def check(z, *v):
 
 print("result of g is : {}".format(add(23,1,3,4,5,6,7,8)))
 
+#varibale number of keywords arguments
 def keywords_args(a, j, l, **kwargs):
     # print()
     print(kwargs)
     return(kwargs)
 keywords_args(1, 2, 3, h=1, b=2, c=3, d=4)
+
+
+#varibale scopes:
+x=5
+def set_x(num):
+    x=num
+    print("hi this x from set_x : {}".format(x))    
+    
+def set_global_x(num):
+    global x
+    print(x)
+    x=num
+    print("hi this x from set_global_x : {}".format(x))
+    
+print(x)
+set_x(50)
+print(x)
+set_global_x(76)
+print(x)
+
     
