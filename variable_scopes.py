efff23bbd0bcb0e6223f436c2bd17238ea51eec6
Dheena-1525 <dheenadhayalan0625@gmail.com>
2025-04-29 09:05:15 +0530

    set variables

---
+++ variable_scopes.py
@@ -0,0 +1,18 @@
+#this code is shown about how varibale scopes works in pythonnlike global and local varibale
+x=5
+def set_local_x(num):
+    x=num
+    print("this is from set_local x : {}".format(x))
+
+def set_global_x(num):
+    global x
+    print("this is from global x : {}".format(x))    
+    x = num
+    print("this x global but its output is local : {}".format(x))
+    
+set_local_x(50)
+#print("this x from set_local_x : {} ".format(x))
+# print(x)
+set_global_x(45)
+#print("this x from set_global_x : {}".format(x))
+# print(x)
